8f5e444ad6ef65c1930807797e220351d9a44af6
Honza Schwarz <cerny.jenda@gmail.com>
2015-10-30 23:59:36 +0100

    email

--- app/mailers/entrant_mailer.rb
+++ app/mailers/entrant_mailer.rb
@@ -4,9 +4,9 @@ class EntrantMailer < ActionMailer::Base
   def registration(entrant)
     @subject = "BĚH DO VRCHU - Informace k platbě startovného "
     @to = entrant.to_mail
-    @body = "Dobrý den,\n\n obdrželi jsme Vaši přihlášku a zasíláme Vám informace k zaplacení startovného:\n\nČíslo účtu: xxxxxxxx/xxxx\nvariabilní symbol: #{entrant.variable_symbol}\nČástka: 100 Kč\n\nPřipsání peněz na náš účet si můžete zkontrolovat na našem webu.\n\nDěkujeme a přejeme kvalitní trénink.\n\nTým BeskydSki\nwww.beskydski.cz"
+    @entrant = entrant
 
-    mail(to: @to, subject: @subject, body: @body) unless @to.blank?
+    mail(to: @to, subject: @subject)
   end
 
 end
