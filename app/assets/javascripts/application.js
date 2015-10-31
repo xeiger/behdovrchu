58a2918ac0468fd02652263fe8908bc5e897fe05
Honza Schwarz <cerny.jenda@gmail.com>
2015-10-31 01:40:54 +0100

    testing jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
 
 // If JavaScript is enabled remove 'no-js' class and give 'js' class
