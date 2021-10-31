97122f8ea24ae9d3789927e331a66a1638ced249
Honza Schwarz <schwarz@insiaoffice.cz>
2021-11-01 00:08:40 +0100

    nastaveni sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => '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',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
