47a971d27f863963679c9f8f047458e980d9a452
Honza Schwarz <schwarz@insiaoffice.cz>
2023-10-10 20:40:16 +0200

    rok 2022

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -30,8 +29,7 @@ ActiveRecord::Schema.define(version: 20191008211645) do
     t.integer  "year",                            null: false
     t.boolean  "male",            default: true
     t.boolean  "climber",         default: false
+    t.index ["variable_symbol"], name: "index_entrants_on_variable_symbol", unique: true, using: :btree
   end
-
-  add_index "entrants", ["variable_symbol"], name: "index_entrants_on_variable_symbol", unique: true, using: :btree
 
 end
