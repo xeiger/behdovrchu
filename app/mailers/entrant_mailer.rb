30b6b2d9805b922623e404f57bd6375375128d88
Honza Schwarz <schwarz@insiaoffice.cz>
2024-10-17 08:35:40 +0200

    spusteni prihlaseni

--- app/mailers/entrant_mailer.rb
+++ app/mailers/entrant_mailer.rb
@@ -4,7 +4,7 @@ class EntrantMailer < ActionMailer::Base
   def registration(entrant)
     @subject = "BĚH DO VRCHU - Informace k platbě startovného "
     @to = entrant.to_mail
-    #@bcc = 'behdovrchu@email.cz'
+    @bcc = 'behdovrchu@email.cz'
     @entrant = entrant
 
     mail(to: @to, subject: @subject, bcc: @bcc)
