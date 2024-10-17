c76cc60e95eceeefdb1b9eccdb81530d5940b808
Honza Schwarz <schwarz@insiaoffice.cz>
2024-10-17 08:16:10 +0200

    odesilani emailu pres GMAIL API

--- app/mailers/entrant_mailer.rb
+++ app/mailers/entrant_mailer.rb
@@ -1,10 +1,10 @@
 class EntrantMailer < ActionMailer::Base
-  default from: "behdovrchu@email.cz"
+  default from: "behdovrchu@gmail.com"
 
   def registration(entrant)
     @subject = "BĚH DO VRCHU - Informace k platbě startovného "
     @to = entrant.to_mail
-    @bcc = 'behdovrchu@email.cz'
+    #@bcc = 'behdovrchu@email.cz'
     @entrant = entrant
 
     mail(to: @to, subject: @subject, bcc: @bcc)
--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,13 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.gmail.com',
   :port => 587,
+  :domain => 'gmail.com',
+  :user_name => 'behdovrchu@gmail.com',
+  :password => 'guibjthlzpskkzhb',
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
+  :open_timeout => 5,
+  :read_timeout => 5 
 }
